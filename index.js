22ae4694ecb57770aa31f8462e36099687c2b25a
Ja Tar <tarnogorskijan@gmail.com>
2024-07-16 15:17:29 +0200

    blob tests

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ function createIframe() {
     fetch('template_WAW_ZACH.html')
         .then(response => response.blob())
         .then(blob => {
-            const blobUrl = URL.createObjectURL(blob);
+            let blobUrl = URL.createObjectURL(blob);
+            blobUrl += '#time=12:02&train_number=222&destination=222&via_stations=222&operator=222&info_bar=222&delay=0&colorbar=%232f353d&colorfont=%23ffffff'; // Proff of concept
             for (let i = 0; i < track_display.length; i++) {
                 const iframe = document.createElement('iframe');
                 iframe.src = blobUrl;
+                iframe.classList.add('iframe_display');
                 track_display[i].appendChild(iframe);
             }
         });
