4dcca8c3d8c7d2d8e631a4474fa1841c3d5e83c6
Ja Tar <tarnogorskijan@gmail.com>
2024-07-22 08:53:44 +0200

    api test, platforms_info

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+window.timetablesAPI_URL = 'https://stacjownik.spythere.eu/api/getActiveTrainList';
+window.timetablesData = [];
+
 function createIframe() {
     const track_display = document.getElementsByClassName('track_display');
 
@@ -8,7 +11,7 @@ function createIframe() {
             let blobUrl = URL.createObjectURL(blob);
 
             for (let i = 0; i < track_display.length; i++) {
-                const { time, train_number, destination, via_stations, operator, info_bar, delay, colorbar, colorfont } = getDataFromTd2(i);
+                const { time, train_number, destination, via_stations, operator, info_bar, delay, colorbar, colorfont } = getProcessedData(i);
                 const blobUrlParm = blobUrl + `#time=${time}&train_number=${train_number}&destination=${destination}&via_stations=${via_stations}&operator=${operator}&info_bar=${info_bar}&delay=${delay}&colorbar=${colorbar}&colorfont=${colorfont}`;
 
                 const iframe = document.createElement('iframe');
@@ -19,19 +22,39 @@ function createIframe() {
         });
 }
 
-function getDataFromTd2(id) {
+function getProcessedData(display_id) {
     let json = {};
     json.time = "12:01";
     json.train_number = "222";
     json.destination = "222";
     json.via_stations = "222";
     json.operator = "222";
-    json.info_bar = `id: ${id}`;
+    json.info_bar = `id: ${display_id}`;
     json.delay = 0;
     json.colorbar = "#2f353d";
     json.colorfont = "#ffffff";
 
     return json;
 }
+
+/* Testing
+function getDataFromAPI() {
+    fetch(window.timetablesAPI_URL)
+        .then(response => response.json())
+        .then(data => {
+            window.timetablesData = data;
+        });
+}
+
+function updateDisplay() {
+
+}
+
+setTimeout(() => {
+    updateDisplay();
+    setInterval(updateDisplay, 240000);
+}, 240000);
+
+*/
 
 createIframe();
