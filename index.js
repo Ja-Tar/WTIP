b5ea134b5bd663d87a9db8893a87dd21859ab8c2
Ja Tar <tarnogorskijan@gmail.com>
2025-05-08 21:10:11 +0200

    LCS Kleszczów

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ window.debug = false;
 window.iframeDebugURL = ""; // example: http://127.0.0.1:5500
 window.platformsAPIDebugBranch = "main"; // example: main
 window.debugTermination = false;
-window.platformsVersionID = "0.0.16";
+window.platformsVersionID = "0.0.17";
 
 if (window.debug === true) {
     console.warn("Debug mode enabled! Change debug to false in index.js before deployment!");
